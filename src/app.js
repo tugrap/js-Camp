5687ab28b7738f9fef8ec83649812dc20994401b
Atugrap-dogan <tugrapali33@gmail.com>
2021-10-23 17:32:06 +0300

    23 Ekim Ders

---
+++ src/app.js
@@ -0,0 +1,41 @@
+console.log("hello world")
+
+
+let dolarBugun = 9.50
+let dolarDun = 9.20
+
+{
+    let dolarDun = 9.10
+}
+
+
+console.log(dolarDun)
+
+const euroDun = 11.2
+//euroDun =11.2
+
+console.log(euroDun)
+
+//array
+let konutKredileri= ["emlak","kamu","normal",15,"özel"]
+
+console.log("<ul>")
+
+
+
+for (let i = 0; i<konutKredileri.length ; i++) {
+    console.log("<li>"+konutKredileri[i]+"</li>")
+    
+}
+
+
+
+
+
+console.log("</ul>")
+
+
+
+
+
+console.log(konutKredileri)
